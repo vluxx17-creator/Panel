5baaf692fd80ca974e67de425ed8ed94dcff0d62
DiMaggio <vluxx17@gmail.com>
2026-08-06 03:40:15 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "8997816663:AAGyPl4aj69g3xeax5AZHmixw7nmhJ5Su
 ADMIN_IDS = _ids(os.getenv("ADMIN_IDS", "8297446667"))
 GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID", "-1004421628533") or 0)
 
-WEBAPP_URL = os.getenv("WEBAPP_URL", "https://vluxx17-creator.github.io/Panel/")
+WEBAPP_URL = os.getenv("https://vluxx17-creator.github.io/Panel/webapp/", "https://vluxx17-creator.github.io/Panel/")
 WEBHOOK_HOST = os.getenv("WEBHOOK_HOST", "")
 WEBHOOK_PATH = os.getenv("WEBHOOK_PATH", "/tg/webhook")
 WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "")
